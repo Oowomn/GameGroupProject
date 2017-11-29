dcea350c7793333b70c757ebeb23b0fc7aae8be7
Thomas Chan <chunshingc@gmail.com>
2017-11-30 03:19:21 +0800

    test

---
+++ project/Assets/Scripts/Camera/mapPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapPoint : MonoBehaviour {
+	public GameObject mappoint;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//mappoint.transform.position.Set(target.transform.position.x,target.transform.position.y+80,target.transform.position.z);
+
+	}
+	void FixedUpdate(){
+		mappoint.transform.position=new Vector3(target.transform.position.x,target.transform.position.y+80,target.transform.position.z);
+
+	}
+}
---
+++ project/Assets/Scripts/Camera/toggledisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggledisplay : MonoBehaviour {
+	public Camera thiscamera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			if (thiscamera.targetDisplay == 0)
+				thiscamera.targetDisplay = 1;
+			else if (thiscamera.targetDisplay == 1)
+				thiscamera.targetDisplay = 0;
+		}
+	}
+
+}
