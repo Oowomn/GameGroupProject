e5eb1cf97f227f332d22def1fe218d523fc278e5
Kenny WU <kenny.wu@mirumagency.com>
2017-11-30 22:03:33 +0800

    Project Initialization

---
+++ Assets/Scripts/Camera/mapPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapPoint : MonoBehaviour {
+	public GameObject mappoint;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//mappoint.transform.position.Set(target.transform.position.x,target.transform.position.y+80,target.transform.position.z);
+
+	}
+	void FixedUpdate(){
+		if(target != null)
+			mappoint.transform.position=new Vector3(target.transform.position.x,target.transform.position.y+80,target.transform.position.z);
+
+	}
+}
---
+++ Assets/_Complete-Game/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class EnemyManager : MonoBehaviour
+    {
+		//private Complete.TankHealth playerHealth;       // Reference to the player's heatlh.
+        public GameObject enemy;                // The enemy prefab to be spawned.
+        public float spawnTime = 3f;            // How long between each spawn.
+        //public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+		private GameObject player; 
+
+        void Start ()
+        {
+			//player = GameObject.FindGameObjectWithTag ("Player");
+            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+			InvokeRepeating ("Spawning", spawnTime, spawnTime);
+        }
+
+
+		private IEnumerator Spawn ()
+        {
+            // If the player has no health left...
+			float remainHP = player.GetComponent<Complete.TankHealth>().CurrentHealth;
+			if(remainHP<= 0f)
+            {
+                // ... exit the function.
+				yield return spawnTime;
+            }
+
+            // Find a random index between zero and one less than the number of spawn points.
+            //int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			Transform a = player.GetComponent<TankManager> ().m_SpawnPoint;
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+			Instantiate (enemy, new Vector3(a.transform.position.x+Random.Range(-30,30),0,a.transform.position.z+Random.Range(-30,30)), a.transform.rotation);
+			yield return spawnTime;
+        }
+		void Spawning ()
+		{
+			player = GameObject.FindGameObjectWithTag ("Player");
+			// If the player has no health left...
+			if (player==null) return ;
+			float remainHP = player.GetComponent<Complete.TankHealth>().CurrentHealth;
+			if(remainHP<= 0f)
+			{
+				// ... exit the function.
+				 return ;
+			}
+
+			// Find a random index between zero and one less than the number of spawn points.
+			//int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			Transform a = player.transform;
+			// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+			Instantiate (enemy, new Vector3(a.transform.position.x+Random.Range(-30,30),0,a.transform.position.z+Random.Range(-30,30)), a.transform.rotation);
+
+		}
+    }
+}
